d7171e00567560aab2c04e3952a6f3d8474b5670
Ankur Gupta <rukna1000@gmail.com>
2018-06-25 12:25:37 -0400

    Moving JS to a separate file

--- js/base.js
+++ js/base.js
@@ -1,6 +1,4 @@
-function setup() {
-    JQuery(function($) {
-        $('#nav').load("/html/templates/nav.html");
-        $('#footer').load("/html/templates/footer.html");
-    })
-}
+$(function() {
+	$('#nav').load("/html/templates/nav.html");
+	$('#footer').load("/html/templates/footer.html");
+});
