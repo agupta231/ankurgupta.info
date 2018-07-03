c7b46ba8ce2ef818b3ad55a972a143d5e04a13d7
Ankur Gupta <rukna1000@gmail.com>
2018-07-03 08:22:45 -0400

    Alpha on ajax requests

--- js/search.js
+++ js/search.js
@@ -1,17 +1,24 @@
-$("document").ready(function () {
-    var data = {
-        "keystring" : "*",
-        "keytag" : "*",
-    };
-    data = $(this).serialize() + "&" + $.param(data);
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        url: "/php/search_site.php",
-        data: data,
-        success: function(response) {
-            alert(response);
-        }
-    });
-    return false;
-});
+function loadResults() {
+	let keystring = "*";
+	let keytag = "*";
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			displayResults(this);			
+		}
+	}
+
+	xhttp.open("POST", "/php/search_site.php", true);
+	xhttp.setRequestHeader("Content-Type", "application/json");
+
+	let sample_data = {
+		"keystring" : "*",
+		"keytag" : "*"
+	};
+	xhttp.send(JSON.stringify(sample_data);
+}
+
+function displayResults(xhttp) {
+	alert(xhttp.responseText);
+}
