f179e54ea4c0064d2f596b389fb0b80c949011c7
Ankur Gupta <rukna1000@gmail.com>
2018-07-03 07:10:13 -0400

    fixed some syntax issues

--- php/search_site.php
+++ php/search_site.php
@@ -1,7 +1,7 @@
 <?php
 
 print(phpinfo());
-$db_file = "/res/posts_db.json";
+$db_file = $_SERVER['DOCUMENT_ROOT'] . "/res/posts_db.json";
 
 # Thank you to https://stackoverflow.com/questions/828870/php-regex-how-to-get-
 # the-string-value-of-html-tag for the code snippet.
@@ -18,13 +18,13 @@ function updateDatabase($db_file) {
 
   # Fill in the "hash"... basically prevents this function if no new data has been added
   $database['lastUpdate'] = 
-    shell_exec("ls -l /html/dynamic/ | grep posts | awk '{print $6 $7 $8}'");
+    shell_exec("ls -l " . $_SERVER['DOCUMENT_ROOT'] . "/html/dynamic/ | grep posts | awk '{print $6 $7 $8}'");
 
   $database['posts'] = array();
   $database['tags'] = array();
 
   # Iterate through the different blog posts and strip the meta data out of them.
-  foreach (glob("/html/dynamic/posts/*.html") as $post) {
+  foreach (glob($_SERVER['DOCUMENT_ROOT'] . "/html/dynamic/posts/*.html") as $post) {
     $current_post_meta = array();
     $post_buffer = file_get_contents($post);
     
@@ -51,7 +51,7 @@ function updateDatabase($db_file) {
 
   arsort($database['tags']);
 
-  $db_handle = fopen($db_file, 'w') or die("Cannot open database file: " . $db_file);
+  $db_handle = fopen($db_file, 'w+') or die("Cannot open database file: " . $db_file);
   fwrite($db_handle, $db_out = json_encode($database));
   fclose($db_handle);
 
@@ -70,7 +70,7 @@ if (file_exists($db_file)) {
   $db_data = json_decode($db_file);
 
   if ($db_data['lastUpdate'] !=
-      shell_exec("ls -l /html/dynamic/ | grep posts | awk '{print $6 $7 $8}'")) {
+      shell_exec("ls -l " . $_SERVER['DOCUMENT_ROOT'] . "/html/dynamic/ | grep posts | awk '{print $6 $7 $8}'")) {
 
     $db_data = updateDatabase($db_file);
   }
@@ -78,4 +78,4 @@ if (file_exists($db_file)) {
     $db_data = updateDatabase($db_file);
 }
 
-echo(json_encode($db_data));
+print(json_encode($db_data));
