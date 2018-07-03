b992eae6b5a2b01864ca5ffa015c7d71a97e2c4e
Ankur Gupta <rukna1000@gmail.com>
2018-07-03 08:24:43 -0400

    Fix syntax issues

--- js/search.js
+++ js/search.js
@@ -2,23 +2,25 @@ function loadResults() {
 	let keystring = "*";
 	let keytag = "*";
 
-	var xhttp = new XMLHttpRequest();
+	let xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
 			displayResults(this);			
 		}
-	}
+	};
 
 	xhttp.open("POST", "/php/search_site.php", true);
 	xhttp.setRequestHeader("Content-Type", "application/json");
 
 	let sample_data = {
-		"keystring" : "*",
-		"keytag" : "*"
+		"keystring" : keystring,
+		"keytag" : keytag
 	};
-	xhttp.send(JSON.stringify(sample_data);
+    xhttp.send(JSON.stringify(sample_data));
 }
 
 function displayResults(xhttp) {
 	alert(xhttp.responseText);
 }
+
+loadResults();
